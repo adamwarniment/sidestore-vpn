8825935c8a6ccac74f01bafae0ce5b9dff3d314a
Lan Tian <xuyh0120@outlook.com>
2025-06-21 21:23:42 -0700

    Allow setting tun device name via args

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
+use clap::Parser;
 use smoltcp::wire::{Ipv4Address, Ipv4Packet};
 use std::io::{Read, Write};
 use tun::AbstractDevice;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the TUN interface
+    #[arg(short, long, default_value = "sidestore")]
+    tun_name: String,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
     let mut config = tun::Configuration::default();
-    config.tun_name("sidestore");
+    config.tun_name(&args.tun_name);
     config.up();
 
     let mut dev = tun::create(&config)?;
@@ -14,7 +25,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("Failed to set destination address");
     dev.enabled(true).expect("Failed to enable interface");
 
-    println!("TUN device is up. Waiting for packets...");
+    println!("TUN device \"{}\" is up", args.tun_name);
 
     let mut buf = [0u8; 1504]; // MTU of 1500 + 4 bytes for header
 
